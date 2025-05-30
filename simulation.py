c88f8c84422e025c50161eac4b259ad1a1532167
cormactaylor <cormacmtaylor@gmail.com>
2025-05-30 15:09:52 -0400

    start of clean up

--- simulation.py
+++ simulation.py
@@ -1,51 +1,75 @@
 import math
+import util
 
 # get largest solution
 # given: a and b are positive and c is negitive
 def solve_quadratic(a, b, c):
-    disc = b**2 - 4*a*c
-    assert disc >= 0
-    
-    x = (-b + math.sqrt(disc)) / (2*a)
-    assert x >= 0
-    return x
-
-def mps_to_mph(x):
-    return x * 3600 / 1609.34
-
-def m_to_ft(x):
-    return x * 3.28084
+  disc = b**2 - 4*a*c
+  assert disc >= 0
+  
+  x = (-b + math.sqrt(disc)) / (2*a)
+  assert x >= 0
+  return x
 
 # description:
   # no-turn and no-lane-change intersection with traffic light
 # assume:
   # infinite cars in queue
-  # "through" := car breaking the stop line's plane
+  # "through" := car being on both sides of the stop line's plane
   # no variance (all drivers act perfectly average)
 def throughput_basic(speed_lim, t_green, t_yellow, car_len, stop_spacing, go_threshold, acc):
-    # time
-    t_total = t_green + t_yellow
-    t_to_speed_limit = speed_lim / acc
-    t_acc = min(t_to_speed_limit, t_total)
-    t_limit = t_total - t_acc
-    
-    # distance
-    d_car = stop_spacing + car_len
-    d_acc = (0.5 * acc * t_acc**2)
-    d_limit = speed_lim * t_limit
+  # time
+  t_total = t_green + t_yellow
+  t_to_speed_limit = speed_lim / acc
+  t_acc = min(t_to_speed_limit, t_total)
+  t_limit = t_total - t_acc
+  
+  # distance
+  d_car = stop_spacing + car_len
+  d_acc = (0.5 * acc * t_acc**2)
+  d_limit = speed_lim * t_limit
 
-    if stop_spacing >= go_threshold:
-        # enough space to accelerate immediately
-        # solved in distance
-        return math.ceil((d_acc + d_limit) / d_car)
-    else:
-        # must wait to accelerate
-        # solved in time
-        d_to_go_threshold = go_threshold - stop_spacing    
-        a = (2 * d_to_go_threshold) / acc
-        b = (2 * d_car) / acc
-        c = - t_total ** 2
-        return math.ceil(solve_quadratic(a, b, c))
+  if stop_spacing > go_threshold:
+    # enough space to accelerate immediately
+    # solved in distance
+    
+    return math.ceil((d_acc + d_limit) / d_car)
+  
+  else:
+    # must wait to accelerate
+    # solved in time
+    
+    d_to_go_threshold = go_threshold - stop_spacing
+    
+    # this assumes:
+      # i * d_car > d_acc > d_to_go_threshold
+      # this is reasonable under realistic considerations
+    num = t_total + (d_acc / speed_lim) - t_acc
+    den = math.sqrt(2 * d_to_go_threshold / acc) + d_car / speed_lim
+    return math.ceil(num / den)
+
+# This is the exausive handling but the condition to decide the equation seems to depend on the result 
+    # if d_acc > d_to_go_threshold:
+    #   if distance traveled by last car is < d_acc:
+    #     a = 2 * d_to_go_threshold / acc
+    #     b = 2 * d_car / acc
+    #     c = -1 * (t_total ** 2)
+    #     return math.ceil(solve_quadratic(a, b, c))
+    #   else:
+    #     num = t_total + (d_acc / speed_lim) - t_acc
+    #     den = math.sqrt(2 * d_to_go_threshold / acc) + d_car / speed_lim
+    #     return math.ceil(num / den)
+    # else: 
+    #   if distance traveled by last car is < d_acc:
+    #     a = (t_acc + (d_to_go_threshold - d_acc) / speed_lim) ** 2
+    #     b = 2 * d_car / acc
+    #     c = -1 * (t_total ** 2)
+    #     return math.ceil(solve_quadratic(a, b, c))
+    #   else:
+    #     print("hit")
+    #     num = t_total + (d_acc / speed_lim) - t_acc
+    #     den = t_acc + (d_to_go_threshold - d_acc + d_car) / speed_lim
+    #     return math.ceil(num / den)
 
 def main():
     speed_limit = 11                # m/s
@@ -66,12 +90,12 @@ Stoplight situation:
     no-turn and no-lane-change intersection with traffic light
     
 Given conditions:
-    Speed limit ----------- {mps_to_mph(speed_limit)} mph
+    Speed limit ----------- {util.mps_to_mph(speed_limit)} mph
     Time green ------------ {green_time} s
     Time yellow ----------- {yellow_time_multiple * speed_limit} s
-    Car lengths ----------- {m_to_ft(car_length)} ft
-    Stopped spacing ------- {m_to_ft(stopped_car_spacing)} ft
-    Space to accelerate --- {m_to_ft(go_threshold)} ft
+    Car lengths ----------- {util.m_to_ft(car_length)} ft
+    Stopped spacing ------- {util.m_to_ft(stopped_car_spacing)} ft
+    Space to accelerate --- {util.m_to_ft(go_threshold)} ft
     Acceleration ---------- {acceleration} m/s^2
     
 Throughput: {throughput_basic(speed_limit, green_time, speed_limit * yellow_time_multiple, car_length, stopped_car_spacing, go_threshold, acceleration)}
---
+++ util.py
@@ -0,0 +1,11 @@
+def m_to_ft(m):
+    return m * 3.28084
+
+def ft_to_m(ft):
+    return ft / 3.28084
+
+def mps_to_mph(mps):
+    return mps * 2.23694
+
+def mph_to_mps(mph):
+    return mph / 2.23694
